8e3994157bcafe0ceb3d818f0a5bf769cab03d76
Eduardo Borges <duducaixetaa@gmail.com>
2023-02-25 10:00:53 -0300

    Quase Finalizado

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,30 +1,35 @@
 import "./search.css";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import {FaThermometerThreeQuarters, FaWater, FaWind} from "react-icons/fa";
 
 
 export default function Search() {
-  const [city, setCity] = useState("");
+  const [city, setCity] = useState("Brasília");
   const [data, setData] = useState("");
-  const [icon, setIcon] = useState("")
+  const [icon, setIcon] = useState("");
   const apikey = "1a9678cc5b312cca6f5648bf8ecbc4e1";
   
   
   
     
-     
+useEffect(()=>{
+  submit()},[]);
     
-  
 
   function submit() {
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apikey}&units=metric`;
     fetch(url)
       .then((response) => response.json())
-      .then((data) => {setData(data)})
-      console.log (data)
-        setIcon(`http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`)
-      
+      .then((data) => {
+        setData(data);
+        setIcon(
+          `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`
+        );
+        console.log(data);
+      });
   }
-  
+ 
+ 
 
   return (
     <div className="container">
@@ -37,9 +42,16 @@ export default function Search() {
         <button onClick={submit}>Procurar</button>
       </div>
       <div className="banner">
-          <h3>{data.name}</h3>
-          <img  src={icon} ></img>
+          <div className="name_icon">
+            <h3>{data.name}</h3>
+            <img  src={icon} ></img>
+          </div>
+          <div className="infos">
+            <p>Temperatura  {Math.floor(data.main?.temp)} ºC <FaThermometerThreeQuarters/> </p>
+            <p>Velocidade do Vento  {data.wind?.speed} <FaWind/></p>
+            <p>Humidade   {data.main?.humidity} % <FaWater/></p>
+          </div>
       </div>
     </div>
   );
-}
+  }
